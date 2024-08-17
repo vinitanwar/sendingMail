ca1e56c82c8e82be396965388c1a38cbef6b7272
Vinodtanwar <Futureittouch@gmail.com>
2024-08-17 14:54:02 +0530

    newone

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.post("/sendmail", async (req, res) => {
 
         
         ${Address ? '<p><strong> Address :</strong> ' + Address + '</p>' : ''}
-        ${skype_id ? '<p><strong> Skype Id :</strong> ' + skype_id + '</p>' : ''}
+    
 
 
         ${selectedCity ? '<p><strong>  Selected City :</strong> ' + selectedCity + '</p>' : ''}
